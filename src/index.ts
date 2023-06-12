8f24bd13f880203fdbc16c1fbd27896dadcb8a7b
Vishnu <vpgit123@gmail.com>
2023-06-12 12:55:09 +1000

    added swagger

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,25 @@
 import express, { Request, Response } from "express";
+import cors from "cors";
+import morgan from "morgan"
+const swaggerUi = require('swagger-ui-express'),
+swaggerDocument = require('../swagger.json');
+
+
 
 export const PORT = 8000;
 
 // Setup app
 export const app = express();
 app.use(express.json());
+app.use(cors());
+app.use(morgan("tiny"));
 
+
+app.use(
+    '/api-docs',
+    swaggerUi.serve, 
+    swaggerUi.setup(swaggerDocument)
+  );
 // Routes
 app.get("/", (req: Request, res: Response) => {
   res.status(200).json({ message: "Sample Route Test" });
